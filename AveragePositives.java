07e90a33a5ed197753948135d20a28e98ef5ee02
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2021-11-29 17:28:59 -0800

    Completed AveragePositives.java, Started PairSelect.java

--- AveragePositives.java
+++ AveragePositives.java
@@ -4,3 +4,34 @@
  * @author Natalie Young
  * @since 2021-11-09
  */
+
+class AveragePositives
+{
+	/**
+	 * Prints average of command-line args strictly greater than 0
+	 * when parsed as doubles; produces 0 if no greater numbers
+	 */
+	public static void main(String[] args)
+	{
+		double sum = 0.0;
+		int totalArgs = 0;
+		double strValue = 0.0;
+		double mean = 0.0;
+
+		for (String arg : args)
+		{
+			strValue = Double.valueOf(arg);
+			
+			if (strValue > 0.0)
+			{
+				sum += strValue;
+
+				totalArgs += 1;
+
+				mean = sum / totalArgs;
+			}
+		}
+
+		System.out.println(mean);
+	}
+}
---
+++ PairSelect.java
@@ -0,0 +1,9 @@
+/**
+ * Programming Assignment 6
+ *
+ * @author Natalie Young
+ * @since 2021-11-09
+ */
+
+class Pair
+{}
